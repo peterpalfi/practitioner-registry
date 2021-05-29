27d614b90b02349619d837ce192dc6c1ba263ae8
Pálfi Péter <h987580@stud.u-szeged.hu>
2021-05-30 00:22:02 +0200

    data-table

--- src/app/pages/data-table/data-table.component.ts
+++ src/app/pages/data-table/data-table.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss']
 })
-export class DataTableComponent implements OnInit {
+export class DataTableComponent implements AfterViewInit {
+  displayedColumns = ['active', 'edit', 'delete'];
+  //'Contact', 'Address', 'Gender', 'Birth Date', 'Qualification', 'Spoken Language'
+  dataSource = new MatTableDataSource<any>();
 
-  constructor() { }
+  constructor(private afs: AngularFirestore, public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.afs.collection<any>('Practitioners').valueChanges().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+    })
+  }
+ 
+  openDialog(_data: any): void {
+    //const dialogRef = this.dialog.open(EditDia)
+  }
+
+  addPractitioner() {
+
+  }
+
+  trackByUid(index: any, item: any) {
+    return item.uid;
   }
 
 }

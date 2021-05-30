b40eff894d15843cc7926469a1a9d7678e1ea486
Pálfi Péter <h987580@stud.u-szeged.hu>
2021-05-30 19:52:28 +0200

    home route

--- src/app/pages/data-table/data-table.component.ts
+++ src/app/pages/data-table/data-table.component.ts
@@ -2,6 +2,7 @@ import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { FbBaseService } from 'src/app/services/fb-base/fb-base.service';
 import { Practitioner } from 'src/app/shared/models/practitioner';
 import { DataAddEditComponent } from '../data-add-edit/data-add-edit.component';
@@ -15,11 +16,15 @@ import { DataDeleteComponent } from '../data-delete/data-delete.component';
 export class DataTableComponent implements AfterViewInit {
   displayedColumns: string[] = ['name', 'gender', 'birthDate', 'contact', 'address', 'qualification', 'active', 'actions'];
   dataSource = new MatTableDataSource<any>();
+  homeRoute = false;
 
   @ViewChild(MatSort)
   sort: MatSort = new MatSort;
 
-  constructor(private service: FbBaseService<Practitioner>, public dialog: MatDialog) { }
+  constructor(private service: FbBaseService<Practitioner>, public dialog: MatDialog, private router: Router) {
+    this.homeRoute = (this.router.url == "/home");
+    if (this.homeRoute) this.displayedColumns.splice(-1,1);
+  }
 
   ngAfterViewInit() {
     this.service.get("Practitioners").subscribe(data => {
@@ -48,17 +53,6 @@ export class DataTableComponent implements AfterViewInit {
     } else {
       dialogRef = this.dialog.open(DataAddEditComponent, {data: data});
     }
-    
-    /* dialogRef.afterClosed().subscribe(
-      (practitioner: Practitioner) => {
-        if (true) {
-          this.service.add("Practitioners", practitioner);
-        }
-      },
-      (err) => {
-        console.warn(err);
-      }
-    ); */
   }
 
   addDummyPractitioner() {
